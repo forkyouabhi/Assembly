d885bf68c4831d51e497bdccdaaf64db2b80e798
ForkYouAbhi <abhijeetsingh3766@gmail.com>
2025-02-28 15:43:14 -0500

    fixed

--- Assignment_2/CalculationExponents.s
+++ Assignment_2/CalculationExponents.s
@@ -1,11 +1,5 @@
-# The code is pretty straightforward. We first compute 2^3, then add 2 to the result. Next, we compute (10^7) and store the result in the  res  variable. Finally, we print the result using  printf . 
-# Compile and run the program: 
-# $ as -o CalculationExponents.o CalculationExponents.s
-# $ gcc -o CalculationExponents CalculationExponents.o
-# $ ./CalculationExponents
-
 .section .data
-fmt:    .string "Result: %ld\n"
+fmt:    .string "Result of ((2^3)+2)^7 = %ld\n"
 
 .section .bss
 res:    .quad 0
@@ -15,44 +9,59 @@ res:    .quad 0
 .extern printf
 
 main:
-    # Compute 2^3
-    movq $2, %rax
-    movq $3, %rcx
-    call power
+  # Compute 2^3
+   movq $2, %rax
+   movq $3, %rcx
+   call power
 
-    # Add 2 (result = 10)
-    addq $2, %rax
+  # Add 2 (result = 8+2 = 10)
+   addq $2, %rax
 
-    # Compute (10^7)
-    movq $7, %rcx
-    call power
+  # Compute (10^7)
+   movq $7, %rcx
+   call power
 
-    # Store result in res
-    movq %rax, res
+  # Store result in res
+   movq %rax, res
 
-    # Print result using printf
-    movq $fmt, %rdi    # Format string
-    movq res, %rsi     # Value to print
-    xorq %rax, %rax    # Clear RAX for variadic function
-    call printf
+  # Print result using printf
+   movq $fmt, %rdi   # Format string
+   movq res, %rsi    # Value to print
+   xorq %rax, %rax   # Clear RAX for variadic function
+   call printf
 
-    # Exit
-    movq $60, %rax
-    xorq %rdi, %rdi
-    syscall
+  # Exit
+   movq $60, %rax
+   xorq %rdi, %rdi
+   syscall
 
 power:
-    # Compute %rax^%rcx (Exponentiation by Squaring)
-    movq %rax, %rbx  # Base
-    movq $1, %rax    # Result
+  # Compute %rax^%rcx (Exponentiation by Squaring)
+   pushq %rbx        # Save rbx
+   movq %rax, %rbx   # Base in rbx
+   movq $1, %rax     # Initialize result to 1
+
 .loop:
-    testq %rcx, %rcx
-    jz .done
-    imulq %rbx, %rax  # Multiply result by base
-    loop .loop
+   testq %rcx, %rcx  # Check if exponent is zero
+   jz .done
+   
+   testq $1, %rcx    # Check if exponent is odd
+   jz .even
+   
+   # If odd, multiply result by base
+   imulq %rbx, %rax
+   
+.even:
+   # Square the base
+   imulq %rbx, %rbx
+   
+   # Divide exponent by 2
+   shrq $1, %rcx
+   
+   jnz .loop        # Continue if exponent not zero
+
 .done:
-    ret
+   popq %rbx        # Restore rbx
+   ret
 
-.section .note.GNU-stack,"",@progbits 
- 
-
+.section .note.GNU-stack,"",@progbits
